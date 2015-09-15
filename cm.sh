9cb51f73b1874dcbbfbaed8d2c27e1deb63bed51
haoqiang <hao.qiang@harmony.com>
2015-09-15 15:00:19 +0800

    update to linux-4.2

--- cm.sh
+++ cm.sh
@@ -5,6 +5,6 @@ aclocal # generate aclocal.m4
 autoheader
 autoconf # generate configure based on configure.ac
 automake --add-missing # generate Makefile.in based on Makefile.am and aclocal.m4
-# ./configure CC=arm-qhao-linux-gnueabi-gcc CXX=arm-qhao-linux-gnueabi-g++ --host=arm --prefix=/home/hao-64/Desktop/arm/qemu_img
-# ./configure --prefix=/home/hao-64/Desktop/arm/qemu_img
+# ./configure CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ --host=arm --prefix=/home/hao/Desktop/arm/qemu_img
+# ./configure --prefix=/home/hao/Desktop/arm/qemu_img
 
