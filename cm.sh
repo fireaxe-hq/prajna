b0df86cee3da7a0fc4b940af609995572ee408dc
Marco Hao <fireaxe.hq@outlook.com>
2016-05-14 22:52:05 +0800

    update for vlx

--- cm.sh
+++ cm.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+#BASE_PATH=/home/hao/Desktop/arm
+BASE_PATH=/home/qemu
+
 autoscan # check new or updat4d files
 aclocal # generate aclocal.m4
 autoheader
 autoconf # generate configure based on configure.ac
 automake --add-missing # generate Makefile.in based on Makefile.am and aclocal.m4
-# ./configure CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ --host=arm --prefix=/home/haoqiang/Desktop/arm/zynq_qemu
-# ./configure --prefix=/home/haoqiang/Desktop/arm/zynq_qemu
+ ./configure CC=arm-linux-gnueabi-gcc CXX=arm-linux-gnueabi-g++ --host=arm --prefix=${BASE_PATH}/zynq_qemu
+# ./configure --prefix=${BASE_PATH}/zynq_qemu
 
